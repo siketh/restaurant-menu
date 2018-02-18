98f4522735235ba17b0b7571405457b5a95b376a
Trevor Roman <troman360@gmail.com>
2018-02-18 12:48:40 -0500

    Implemented crud operations for menu items

--- project.py
+++ project.py
@@ -77,32 +77,100 @@ def deleteRestaurant(restaurant_id):
 
 @app.route('/restaurants/<int:restaurant_id>/')
 def restaurant(restaurant_id):
-    return render_template("restaurant.html", restaurant=mock_data.restaurant)
+    restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
+    return render_template("restaurant.html", restaurant=restaurant)
 
 
 @app.route('/restaurants/<int:restaurant_id>/menu')
 def menu(restaurant_id):
-    return render_template("menu.html", restaurant=mock_data.restaurant, menu_items=mock_data.items)
+    restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
+    menu_items = session.query(MenuItem).filter_by(restaurant_id=restaurant_id)
+
+    return render_template("menu.html", restaurant=restaurant, menu_items=menu_items)
 
 
 @app.route('/restaurants/<int:restaurant_id>/menu/new', methods=['GET', 'POST'])
 def newMenuItem(restaurant_id):
-    return render_template("new-menu-item.html", restaurant=mock_data.restaurant)
+    if request.method == "POST":
+        name = request.form["name"]
+        description = request.form["description"]
+        price = request.form["price"]
+        course = request.form["course"]
+
+        new_menu_item = MenuItem(
+            name=name, description=description, price=price, course=course, restaurant_id=restaurant_id)
+
+        session.add(new_menu_item)
+        session.commit()
+
+        return redirect(url_for('menu', restaurant_id=restaurant_id))
+    elif request.method == "GET":
+        restaurant = session.query(
+            Restaurant).filter_by(id=restaurant_id).one()
+
+        return render_template("new-menu-item.html", restaurant=restaurant)
+    else:
+        return "Request method [{}] not supported!".format(request.method)
 
 
 @app.route('/restaurants/<int:restaurant_id>/menu/<int:menu_item_id>/edit', methods=['GET', 'POST'])
 def editMenuItem(restaurant_id, menu_item_id):
-    return render_template("edit-menu-item.html", restaurant=mock_data.restaurant, menu_item=mock_data.item)
+    if request.method == "POST":
+        menu_item = session.query(MenuItem).filter_by(
+            id=menu_item_id, restaurant_id=restaurant_id).one()
+
+        if request.form["name"]:
+            menu_item.name = request.form["name"]
+        if request.form["description"]:
+            menu_item.description = request.form["description"]
+        if request.form["price"]:
+            menu_item.price = request.form["price"]
+        if request.form["course"]:
+            menu_item.course = request.form["course"]
+
+        session.add(menu_item)
+        session.commit()
+
+        return redirect(url_for('menu', restaurant_id=restaurant_id))
+    elif request.method == "GET":
+        restaurant = session.query(
+            Restaurant).filter_by(id=restaurant_id).one()
+        menu_item = session.query(MenuItem).filter_by(
+            restaurant_id=restaurant_id, id=menu_item_id).one()
+
+        return render_template("edit-menu-item.html", restaurant=restaurant, menu_item=menu_item)
+    else:
+        return "Request method [{}] not supported!".format(request.method)
 
 
 @app.route('/restaurants/<int:restaurant_id>/menu/<int:menu_item_id>/delete', methods=['GET', 'POST'])
 def deleteMenuItem(restaurant_id, menu_item_id):
-    return render_template("delete-menu-item.html", restaurant=mock_data.restaurant, menu_item=mock_data.item)
+    if request.method == "POST":
+        menu_item = session.query(MenuItem).filter_by(
+            id=menu_item_id, restaurant_id=restaurant_id).one()
+
+        session.delete(menu_item)
+        session.commit()
+
+        return redirect(url_for('menu', restaurant_id=restaurant_id))
+    elif request.method == "GET":
+        restaurant = session.query(
+            Restaurant).filter_by(id=restaurant_id).one()
+        menu_item = session.query(MenuItem).filter_by(
+            restaurant_id=restaurant_id, id=menu_item_id).one()
+
+        return render_template("delete-menu-item.html", restaurant=restaurant, menu_item=menu_item)
+    else:
+        return "Request method [{}] not supported!".format(request.method)
 
 
 @app.route('/restaurants/<int:restaurant_id>/menu/<int:menu_item_id>/')
 def menuItem(restaurant_id, menu_item_id):
-    return render_template("menu-item.html", restaurant=mock_data.restaurant, menu_item=mock_data.item)
+    restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
+    menu_item = session.query(MenuItem).filter_by(
+        restaurant_id=restaurant_id, id=menu_item_id).one()
+
+    return render_template("menu-item.html", restaurant=restaurant, menu_item=menu_item)
 
 
 if __name__ == '__main__':
